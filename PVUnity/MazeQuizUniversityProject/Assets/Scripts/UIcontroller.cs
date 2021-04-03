cb6132941220c3d9657d48145547954d2918bb23
bazukax <kristijonas9871@gmail.com>
2021-04-03 20:44:56 +0300

    added enemies

---
+++ PVUnity/MazeQuizUniversityProject/Assets/Scripts/UIcontroller.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIcontroller : MonoBehaviour
+{
+    public GameObject player;
+   public void OnCorrectAnswerClick(GameObject enemy)
+    {
+        Destroy(enemy);
+        player.GetComponent<PlayerController>().enabled = true;
+    }
+    public void OnWrongAnswerClick(GameObject enemy)
+    {
+        enemy.GetComponent<Quiz>().Restart();
+    }
+}
