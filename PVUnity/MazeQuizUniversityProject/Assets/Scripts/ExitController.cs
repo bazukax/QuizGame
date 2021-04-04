87b92624b5fd7df3be6008c1ae4d7ac39a0a18d8
bazukax <kristijonas9871@gmail.com>
2021-04-04 10:38:00 +0300

    bug fixes

---
+++ PVUnity/MazeQuizUniversityProject/Assets/Scripts/ExitController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitController : MonoBehaviour
+{
+    public int nextLevelId = 0;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            ChangeLevel();
+        }
+    }
+    void ChangeLevel()
+    {
+        Application.LoadLevel(nextLevelId);
+    }
+}
