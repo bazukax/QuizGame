d4e04ed16fa861ad54f72c52873c1fdc69988ef4
Edviuxas <sutkuse@gmail.com>
2021-04-05 16:13:16 +0300

    Added timer

---
+++ PVUnity/MazeQuizUniversityProject/Assets/timerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timerScript : MonoBehaviour
+{
+    public Text timer;
+    public float time = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        time += 1 * Time.deltaTime;
+        timer.text = time.ToString();
+        
+    }
+}
