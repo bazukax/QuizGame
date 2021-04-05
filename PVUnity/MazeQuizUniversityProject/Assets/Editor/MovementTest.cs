1ebc5b8ed68b873e85b8d097a60384a9cd362b0b
Adomas <adomas.slanciauskas@stud.vgtu.lt>
2021-04-05 17:10:23 +0300

    test for movement

---
+++ PVUnity/MazeQuizUniversityProject/Assets/Editor/MovementTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class MovementTest
+{
+    [Test]
+    public void TestMovement()
+    {
+        //ARRANGE
+        GameObject obj = new GameObject();
+        obj.AddComponent<PlayerController>();
+        //ACT
+
+
+        //ASSERT
+        Assert.IsTrue(obj.GetComponent<PlayerController>().runSpeed >= 0);
+    }
+}
